c97569ec46dd9e01f32e699e254fdac09293d187
kkoyamaa <yuka_koyama@icloud.com>
2021-12-03 20:55:47 +0900

    :sparkles: 書籍検索　カテゴリ複数検索

--- api-server/src/main/java/com/example/booksearchapp/forms/SearchForm.java
+++ api-server/src/main/java/com/example/booksearchapp/forms/SearchForm.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Setter
 
 public class SearchForm {
-    String nameParam;
-    String labelParam;
-    List<String> categoryParam;
+    String name;
+    String label;
+    List<String> category;
 }
